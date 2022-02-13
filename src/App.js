1f71e016b738260dd770faeb341452c440c121c9
MrNeedToKnow <andy_chen123@hotmail.com>
2022-02-13 15:31:11 -0500

    data imported, working on getting the average with reduce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-
+import React, {useState, useEffect}from 'react'
+import Axios from 'axios'
 import './App.css';
+import {Card} from 'react-bootstrap'
 
 function App() {
+  const [students, setStudents] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  // fetch student data and store it in state
+  useEffect(()=>{
+    async function fetchStudentData(){
+      try{
+        const res = await Axios.get('https://api.hatchways.io/assessment/students')
+        setStudents(res.data.students)
+        setLoading(false)
+        console.log(students)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    fetchStudentData()
+  },[])
+
+
+  // calculate Average Score
+  function getAverageGrade(gradeArray){
+    gradeArray.reduce((previousValue, currentValue) => )
+  }
+
+
   return (
+    <>
+      {students.map((student, index) => {
+        return(
+          <Card className='studentCardContainer' key={index}>
+            <Card.Img className='studentImg' src={student.pic} ></Card.Img>
 
-    <h1>hello world</h1>
+            <h3>{student.firstName} {student.lastName}</h3>
 
+            <p>Email: {student.email}</p>
+            <p>Company: {student.company}</p>
+            <p>Skill: {student.skill}</p>
+            <p>Average:</p>
+          </Card>
+        )
+      })
+
+      }
+    </>
   )
 }
 
